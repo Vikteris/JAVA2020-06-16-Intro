a2a47ce525ca322885abeb539933edead76d2f00
Vikteris <63242034+Vikteris@users.noreply.github.com>
2020-06-23 02:25:25 +0300

    mini hintai

---
+++ src/lt/bit/java/Hintai.java
@@ -0,0 +1,45 @@
+//package lt.bit.java;
+//
+//public class Hintai {
+    /*
+    *blokus galima kurti ten kur norim,
+    * ciklo kintamasis i neprieinamas ciklo bloko isoreje
+    * PASISKAITYT APIE SCOPE NAUDOJIMA, KUr bloku viduje naudojamas tas
+    *
+    *
+    *
+    * public enum Color {
+    BLACK, RED, YELLOW, GREEN;// jei kuriasi nauja reiksme, niekada negalima rašyti pradžioj
+    //o visada i gala, kad jei panaudojai jau esamnas reiksmes, kad nepakeist ju eiles numerio ir nesumaisyt
+    * enum yra- tai sukurtas duomenu tipas, kurie turi fiksuotas reiksmes . enumai nekintamos reikšmes
+    *
+    *
+    *
+    * geteris seteris yra naudojama kaip padaromi clases kintamijei "private"
+    * get, .set naudojamas kai nori gaut indformacxija jei klases informacija kuri padaryta "private", panaudoti jos nesugadinant
+    *
+    *
+    *
+    * this. žodelis- klases kintamasis, klases vardas/ kintmaojo reikšme.
+    *
+    *
+    *
+    * konstruktorius kviečiam su new, nurodom klase ir pn, defaultinis konstruktoerius buna new...() <- cia defaultinis
+    *
+    *
+    *
+    * static int stdMetoddas = 0;// jei reiksmes yra klases laukai, tai jos visada buna 0. nebutina rašyti =0;
+    *
+    *
+    * kai classeje yra pagrindinis metodas,ir  darant kita, kuriamae bus itraukiama informacija
+    // is ankstesniuju metodu/fukciju, užtenka parašyti tik static
+    *
+    *
+    *
+    * scanneris turi skirtingus metodus. ju daug
+
+     */
+
+
+
+//}
